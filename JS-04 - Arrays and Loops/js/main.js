9e61ff8df3a00ffccf4b14c94f68cb4650c5d0ea
MTSC <teresa.scontla@gmail.com>
2023-03-27 17:15:24 -0600

    Se agregó ejercicio 1 completo con las tres partes y los docs del ejercicio 2.

---
+++ JS-04 - Arrays and Loops/js/main.js
@@ -0,0 +1,19 @@
+/*Parte1*/
+let nombres = ["Sofia","David","Juan"];
+nombres = ["Sofia","David","Juan"];
+console.log(nombres);
+
+
+/*Parte2*/
+nombres = ["Sofia","David","Juan"];
+nombres.push("Sara","Agustin");
+nombres.shift("Sofia");
+console.log(nombres);
+
+/*Parte3*/
+nombres = ["Sofia","David","Juan"];
+nombres.push("Sara","Agustin");
+nombres.shift("Sofia");
+nombres.splice(1,0,"Renata");
+nombres.push("Elena");
+console.log(nombres);
